10e33fee72a24918b998cb458c8690cf29c3cbeb
Gavin Sharkey <gavinsharkey2016@gmail.com>
2020-03-03 19:49:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,10 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if name_hash.empty?
+    nil
+  else
+    keys = name_hash.collect { |key, value| key }
+    values = name_hash.collect { |key, value| value }
+  end
 end
